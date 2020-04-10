3477192a065098dcefbc1dd5d719a5fbbeaa3e38
Paweł Szarapka <you@example.com>
2020-04-10 17:07:22 +0200

    commit

--- Page/main.js
+++ Page/main.js
@@ -1,5 +1,8 @@
 const bar = document.querySelector('.bar:nth-child(3)');
 const menu = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+
 menu.addEventListener('click', () => {
-    bar.classList.toggle('active');
+    bar.classList.toggle('active-bar');
+    nav.classList.toggle('active-nav');
 })
