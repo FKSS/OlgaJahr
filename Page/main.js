f2e6afc651a831a5530d8553989d0f6d3ff437c3
Paweł Szarapka <you@example.com>
2020-04-13 14:06:51 +0200

    commit

--- Page/main.js
+++ Page/main.js
@@ -1,10 +1,13 @@
 const bar = document.querySelector('.bar:nth-child(3)');
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('nav');
-const menuBars = document.querySelectorAll('.bar');
+const menuBars = [...document.querySelectorAll('.bar')];
 
 
 menu.addEventListener('click', () => {
+    menuBars.forEach(bar => {
+        bar.classList.toggle('active-menu');
+    })
     bar.classList.toggle('active-bar');
     nav.classList.toggle('active-nav');
 })
